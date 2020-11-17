a0555799aab6a253885d40f9d58e7cad527f5f85
timkleintk <timvdlr@gmail.com>
2020-11-17 12:29:20 +0100

    got libs working

--- raceGame3001/main.cpp
+++ raceGame3001/main.cpp
@@ -1,10 +1,28 @@
 // main cpp file or something
 
+// copy pasted from sfml-dev.org
+
+#include <SFML/Graphics.hpp>
+
 int main()
 {
-	
-	// main function i guess
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-	return 0;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
